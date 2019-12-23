dd9029c225df529e3c6f21e03dcb167bd5d85f95
bowingman <ngmaster.sag@gmail.com>
2019-12-23 22:36:38 +0800

    added test for PutVehicle

--- backend/Car-Rental-System-Test/VehiclesControllerTest.cs
+++ backend/Car-Rental-System-Test/VehiclesControllerTest.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using Car_Rental_System_Test.Config;
 using Xunit.Extensions;
+using System.Globalization;
 
 /**
  * NOTE:
@@ -40,6 +41,50 @@ namespace Car_Rental_System_Test
             new[]
             {
                  new object[] {Guid.Parse("23c07876-a967-4cf0-bf22-0fdeaf7fffff")}
+            };
+
+        public static IEnumerable<object[]> ValidVehicleToBeUpdated =>
+            new[]
+            {
+                new object[] 
+                {
+                    Guid.Parse("23c07876-a967-4cf0-bf22-0fdeaf7beb06"),
+                    new Vehicle
+                    {
+                        Id = 1,
+                        Uuid = Guid.Parse("23c07876-a967-4cf0-bf22-0fdeaf7beb06"),
+                        Manufacturer = "Bugatti",
+                        Model = "Veyron 16.4 Super Sport",
+                        Year = 2011,
+                        Odometer = 2000.00M,
+                        Registration = "2VEYRON",
+                        FuelType = "Petrol",
+                        TankSize = 100.00M,
+                        CreatedAt = DateTime.ParseExact("2019-12-12 02:10:39", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)
+                    }
+                }
+            };
+
+        public static IEnumerable<object[]> InvalidVehicleToBeUpdated =>
+            new[]
+            {
+                new object[]
+                {
+                    Guid.Parse("23c07876-a967-4cf0-bf22-0fdeaf7fffff"),
+                    new Vehicle
+                    {
+                        Id = 1,
+                        Uuid = Guid.Parse("23c07876-a967-4cf0-bf22-0fdeaf7beb06"),
+                        Manufacturer = "Bugatti",
+                        Model = "Veyron 16.4 Super Sport",
+                        Year = 2011,
+                        Odometer = 4000.00M,
+                        Registration = "3VEYRON",
+                        FuelType = "Petrol",
+                        TankSize = 100.00M,
+                        CreatedAt = DateTime.ParseExact("2019-12-12 02:10:39", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)
+                    }
+                }
             };
 
         public VehiclesControllerTest(ITestOutputHelper outputHelper)
@@ -203,6 +248,86 @@ namespace Car_Rental_System_Test
                         var actual = okResult?.Value as Vehicle;
 
                         Assert.Equal(expected, actual);
+                    }
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Tests <see cref="VehiclesController.PutVehicle(Guid, Vehicle)"/> from <see cref="VehiclesController"/>
+        /// If both <paramref name="uuid"/> and <paramref name="vehicle"/> are valid, an existing vehicle in the context should be updated
+        /// Otherwise, if one of them is invalid (or both are invalid) the corresponding vehicle should not be updated
+        /// </summary>
+        /// <param name="uuid">UUID of the <see cref="Vehicle"/> to be updated</param>
+        /// <param name="vehicle">updated <see cref="Vehicle"/></param>
+        [Theory]
+        [MemberData(nameof(InvalidVehicleToBeUpdated))]
+        [MemberData(nameof(ValidVehicleToBeUpdated))]
+        public async void VehiclesController_PutVehicle(Guid uuid, Vehicle vehicle)
+        {
+            try
+            {
+                var options = new DbContextOptionsBuilder<NMTFleetManagerContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (context = new NMTFleetManagerContext(options))
+                {
+                    context.Database.OpenConnection();
+                    context.Database.EnsureCreated();
+                }
+
+                using (context = new NMTFleetManagerContext(options))
+                {
+                    SeedCollection seedCollection = SeedData.Seed();
+                    context.Vehicles.AddRange(seedCollection.Vehicles);
+                    context.SaveChanges();
+                }
+
+                using (context = new NMTFleetManagerContext(options))
+                {
+                    var controller = new VehiclesController(context);
+                    var actionResult = await controller.PutVehicle(uuid, vehicle);
+
+                    bool validVehicle = await context.Vehicles.AnyAsync(v => v.Uuid == uuid && v.Uuid == vehicle.Uuid);
+
+                    if (validVehicle)
+                    {
+                        Assert.IsType<NoContentResult>(actionResult);
+                        var expected = vehicle;
+                        var actual = await context.Vehicles.SingleOrDefaultAsync(v => v.Uuid == uuid);
+
+                        output.WriteLine(expected.ToString());
+                        output.WriteLine(actual.ToString());
+
+                        Assert.Equal(expected, actual);
+                    }
+                    else
+                    {
+                        Assert.IsNotType<NoContentResult>(actionResult);
+
+                        var invalidVehicle = vehicle;
+                        var actual = await context.Vehicles.SingleOrDefaultAsync(v => v.Uuid == uuid);
+
+                        if (actual == null)
+                        {
+                            actual = await context.Vehicles.SingleOrDefaultAsync(v => v.Uuid == vehicle.Uuid);
+
+                            if (actual == null)
+                            {
+                                // since actual does not refer to any existing vehicle in the context, pass test
+                                Assert.Null(actual);
+                                return;
+                            }
+                        }
+
+                        // ensure that the corresponding vehicle in the context has not been updated
+                        Assert.NotEqual(invalidVehicle.Odometer, actual.Odometer);
+                        Assert.NotEqual(invalidVehicle.Registration, actual.Registration);
                     }
                 }
             }
