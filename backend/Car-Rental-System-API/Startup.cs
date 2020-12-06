2d0b77d33fffeebd6d4a5caed72bccdfc9758944
bowingman <ngmaster.sag@gmail.com>
2020-12-06 21:11:54 +0800

    updated configs

--- backend/Car-Rental-System-API/Startup.cs
+++ backend/Car-Rental-System-API/Startup.cs
@@ -39,7 +39,7 @@ namespace Car_Rental_System_API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
